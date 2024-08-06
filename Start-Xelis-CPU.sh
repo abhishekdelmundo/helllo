1ed5ea9752e878f37347a38b486ea82514b0ee7f
abhishekdelmundo <137968400+abhishekdelmundo@users.noreply.github.com>
2024-08-06 08:39:57 +0530

    Add files via upload

---
+++ Start-Xelis-CPU.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#./SRBMiner-MULTI --algorithm xelishashv2 --pool xel.kryptex.network:7777 --wallet xel:qcd39a5u8cscztamjuyr7hdj6hh2wh9nrmhp86ljx2sz6t99ndjqzqq9qqqqqzpnj3tdcp0t295/MyFirstRig
+
+
+#!/bin/sh
+
+./SRBMiner-MULTI --algorithm xelishashv2 --pool xel.kryptex.network:7777 --wallet xel:qcd39a5u8cscztamjuyr7hdj6hh2wh9nrmhp86ljx2sz6t99ndjqzqq9qqqqqzruse9yk2nqlyz/Rig001
